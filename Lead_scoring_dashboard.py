9ba3eed1d2000719557fcb512f7206d558bcad5a
sdimeji <sdimeji@gmail.com>
2025-05-21 22:11:49 -0400

    Updated

--- Lead_scoring_dashboard.py
+++ Lead_scoring_dashboard.py
@@ -30,7 +30,7 @@ df.dropna(inplace=True)
 
 # add title to your dashboard
 st.title('Lead scoring dashboard')
-st.header(':blue[In this project I implemented a model that can determine if a lead will be converted or not, using key variables, this model will save resources and time for American business, imagine a situation where a team can use least time to achieve higher revenue]')
+st.header(':blue[This project implemented a model that can determine if a lead will be converted or not, using key variables, this model will save resources and time for American business, imagine a situation where a team can use least time to achieve higher revenue]')
 
 tab1, tab2 = st.tabs(["EDA", "ML"])
 
